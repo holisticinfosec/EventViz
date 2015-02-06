4236b5d5ec1d3c1d06ba7cf4a2f1d4c458091a46
Russ McRee <holisticinfosec@gmail.com>
2015-02-05 17:36:50 -0800

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,59 @@
+#EventViz is released under the MIT License (MIT)
+#Copyright (c) 2015 Russ McRee, HolisticInfoSec
+
+library(shiny)
+
+setwd("C:/coding/R/apps/EventViz")
+df <- read.csv("sysmon.csv",header=FALSE,skip=1,row.names=NULL,stringsAsFactors=FALSE,sep=",",
+               col.names=c("Level","DateTime","Source","EventID","TaskCategory","Message"))
+
+# Define UI
+shinyUI(
+        fluidPage(
+                titlePanel("EventViz v0.1"),
+                
+                # Create new row in UI for selectInputs
+                fluidRow(
+                        column(width=1, 
+                               selectInput("Level", 
+                                           "Level:", 
+                                           c("All", 
+                                             unique(as.character(df$Level))))
+                        ),
+                        column(width=2, 
+                               selectInput("DateTime", 
+                                           "Date and Time:", 
+                                           c("All", 
+                                             unique(as.character(df$DateTime))))
+                        ),
+                        column(width=2, 
+                               selectInput("Source", 
+                                           "Source:", 
+                                           c("All", 
+                                             unique(as.character(df$Source))))
+                        ),
+                        column(width=1, 
+                               selectInput("EventID", 
+                                           "Event ID:", 
+                                           c("All", 
+                                             unique(as.character(df$EventID))))
+                        ),
+                        column(width=2, 
+                               selectInput("TaskCategory", 
+                                           "Task Category:", 
+                                           c("All", 
+                                             unique(as.character(df$TaskCategory))))
+                        ),
+                        column(width=2, 
+                               selectInput("Message", 
+                                           "Message:", 
+                                           c("All", 
+                                             unique(as.character(df$Message))))       
+                        )
+                ),
+                # Create a new row for the table.
+                fluidRow(
+                        dataTableOutput(outputId="table")
+                )    
+        )  
+)
